b92fbf6bb5d32d07a9b81c498f0777c4d4063034
flexike <flexike@ukr.net>
2024-04-16 00:51:57 +0300

    +server; +usestates

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,33 @@
 import "./login.sass";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./components/Footer";
 
 const Login = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  function login(e) {
+    e.preventDefault();
+  }
+
   return (
     <section className="login-section">
       <div className="container container-login">
         <div className="login-content">
           <h2>Login</h2>
-          <form action="" autoComplete="off">
+          <form action="" autoComplete="off" onSubmit={login}>
             <input
-              type="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
               placeholder="Username"
               name="username"
               required
             />
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               placeholder="Password"
               name="password"
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,27 @@
 import "./register.sass";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./components/Footer";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function Register(e) {
+    e.preventDefault();
+  }
+
   return (
     <section className="reg-section">
       <div className="container container-reg">
         <div className="reg-content">
           <h2>registration</h2>
-          <form action="" autoComplete="off">
+          <form action="" autoComplete="off" onSubmit={Register}>
             <label htmlFor="username"></label>
             <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               name="username"
               type="text"
               placeholder="Create your username"
@@ -18,6 +29,8 @@ const Register = () => {
             />
             <label htmlFor="email"></label>
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               name="email"
               type="email"
               placeholder="Enter your email"
@@ -25,6 +38,8 @@ const Register = () => {
             />
             <label htmlFor="password"></label>
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               name="password"
               type="password"
               placeholder="Create your password"
